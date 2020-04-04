d824dd234dd2511e999da8957293256f3ee7cd11
Denis_Bednakov <50529176+bednakovdenis@users.noreply.github.com>
2020-04-04 11:13:29 +0300

    Add files via upload

---
+++ INSTALL.sh
@@ -0,0 +1,9 @@
+sudo apt install maven default-jdk default-jre openjdk-8-jdk openjdk-8-jre -y
+
+sudo apt install zlib1g-dev libncurses5-dev lib32z1 lib32ncurses6 -y
+
+chmod + x configure.sh
+
+./configure.sh
+
+java -jar sAINT.jar
---
+++ start.sh
@@ -0,0 +1,2 @@
+java -jar sAINT.jar
+
